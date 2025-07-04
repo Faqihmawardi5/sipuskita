8ba03fa75f993dbf0d840ceec194e483f06e3cf3
Faqih_Mawardi5 <masardi16@outlook.co.id>
2025-07-04 17:30:49 +0700

    Update edit_view.php

--- application/views/buku/edit_view.php
+++ application/views/buku/edit_view.php
@@ -18,6 +18,8 @@
 			    <!-- /.box-header -->
 			    <div class="box-body">
                     <form action="<?php echo base_url('data/prosesbuku');?>" method="POST" enctype="multipart/form-data">
+                    <!-- CSRF Token jika aktif -->
+                    <input type="hidden" name="<?= $this->security->get_csrf_token_name(); ?>" value="<?= $this->security->get_csrf_hash(); ?>" />
                         <div class="row">
                             <div class="col-sm-6">
 								<div class="form-group">
