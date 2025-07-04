bb5de39f65495b0eb8a43cacdfbb65714dc04fb8
Faqih_Mawardi5 <masardi16@outlook.co.id>
2025-07-04 17:34:42 +0700

    Update kat_view.php

--- application/views/kategori/kat_view.php
+++ application/views/kategori/kat_view.php
@@ -27,6 +27,8 @@
 						<div class="box-body">
 							<?php if(!empty($this->input->get('id'))){?>
 							<form method="post" action="<?= base_url('data/katproses');?>">
+							<!-- CSRF Token jika aktif -->
+							<input type="hidden" name="<?= $this->security->get_csrf_token_name(); ?>" value="<?= $this->security->get_csrf_hash(); ?>" />
 								<div class="form-group">
 								<label for="">Nama Kategori</label>
 								<input type="text" name="kategori"  value="<?=$kat->nama_kategori;?>" id="kategori" class="form-control"  placeholder="Contoh : Pemrograman Web" >
@@ -39,6 +41,8 @@
 							<?php }else{?>
 
 							<form method="post" action="<?= base_url('data/katproses');?>">
+							<!-- CSRF Token jika aktif -->
+							<input type="hidden" name="<?= $this->security->get_csrf_token_name(); ?>" value="<?= $this->security->get_csrf_hash(); ?>" />
 								<div class="form-group">
 								<label for="">Nama Kategori</label>
 								<input type="text" name="kategori" id="kategori" class="form-control" placeholder="Contoh : Pemrograman Web" >
