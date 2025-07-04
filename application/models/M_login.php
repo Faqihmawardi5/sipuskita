880118d79b85682467ab3cc08f39214df3ad6481
Faqih_Mawardi5 <masardi16@outlook.co.id>
2025-07-04 16:52:16 +0700

    Update M_login.php

--- application/models/M_login.php
+++ application/models/M_login.php
@@ -1,20 +1,37 @@
 <?php
-if(! defined('BASEPATH')) exit('No direct script acess allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class M_Login extends CI_Model
 {
+    /**
+     * Verifikasi user login berdasarkan username dan password
+     *
+     * @param string $username
+     * @param string $password
+     * @return object|null
+     */
+    public function verify_login($username, $password)
+    {
+        // Gunakan Active Record untuk menghindari SQL injection
+        $query = $this->db->get_where('tbl_login', ['user' => $username], 1);
 
-  function GET_LOGIN($user,$pass)
-  {
-      $row = $this->db->query("SELECT * FROM tbl_login WHERE user ='$user' AND pass = '$pass'");
-      return $row;
-  }
+        if ($query->num_rows() == 1) {
+            $user = $query->row();
 
-  function insertTable($table_name,$data)
-  {
-   $tambah = $this->db->insert($table_name,$data);
-   return $tambah;
-  }
+            // Verifikasi password menggunakan password_verify
+            if (password_verify($password, $user->pass)) {
+                return $user; // Login berhasil
+            }
+        }
 
+        return null; // Login gagal
+    }
+
+    /**
+     * Insert ke tabel manapun
+     */
+    public function insertTable($table_name, $data)
+    {
+        return $this->db->insert($table_name, $data);
+    }
 }
-?>
